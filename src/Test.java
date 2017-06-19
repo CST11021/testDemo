6c6c80ed36a57a770b864fa0909a6d33ac2dee19
wanghz <l>
2017-06-19 21:55:26 +0800

    测试Hello world

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,9 @@
  * Created by whz on 2017/6/19.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
 }
