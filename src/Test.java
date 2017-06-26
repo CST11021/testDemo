a87d900104cbef1dcd4ce5ed3736e9af6a3f4a4f
wb-whz291815 <wanghongzhan4413@dingtalk.com>
2017-06-26 14:11:58 +0800

    第二次将分支fenzhi_v1_20160626添加的代码提交到远程分支上

--- src/Test.java
+++ src/Test.java
@@ -9,5 +9,7 @@ public class Test {
     public void test1() {
         // 我在分支fenzhi_v1_20170626下添加了这行代码
         System.out.print("");
+        // 我在分支fenzhi_v1_20170626下第二次添加了这行代码
+        System.out.print("");
     }
 }
